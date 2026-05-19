82b99a0437f054f753f68833f7b3c0905d2b5667
Gabriel Dias <gabrieldiasagostinho@gmail.com>
2026-05-19 17:38:10 -0300

    Início do Projeto

---
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,20 @@
+package med.voll.api.controller;
+
+
+import med.voll.api.medico.DadosCadastroMedico;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String dados){
+        System.out.println(dados);
+    }
+
+    @GetMapping
+    public void view(@RequestBody String dados){
+        System.out.println(dados);
+    }
+}
